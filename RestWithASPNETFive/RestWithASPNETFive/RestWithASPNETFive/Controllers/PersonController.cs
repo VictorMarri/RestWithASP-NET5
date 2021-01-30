d5a10b37f6afe5887a8ebf7b13acc6980476ff8a
Dijkstra <victormarri@verscontabilidade.com.br>
2021-01-30 00:32:09 -0300

    Model Person Adicionado

---
+++ RestWithASPNETFive/RestWithASPNETFive/RestWithASPNETFive/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETFive.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : Controller
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+
+    }
+}
---
+++ RestWithASPNETFive/RestWithASPNETFive/RestWithASPNETFive/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETFive.Models
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+        public string Gender { get; set; }
+    }
+}
