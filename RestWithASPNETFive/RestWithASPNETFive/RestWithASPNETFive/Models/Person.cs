6baa408f10c6e1b7b0be4e69c195ac3758230de4
Dijkstra <victormarri@verscontabilidade.com.br>
2021-02-02 13:35:06 -0300

    Models alterada, inserção de scripts sql pra popular tabela e simular uma migration

--- RestWithASPNETFive/RestWithASPNETFive/RestWithASPNETFive/Models/Person.cs
+++ RestWithASPNETFive/RestWithASPNETFive/RestWithASPNETFive/Models/Person.cs
@@ -7,7 +7,7 @@ namespace RestWithASPNETFive.Models
     {
         [Column("id")]
         public long Id { get; set; }
-        [Column("fitst_name")]
+        [Column("first_name")]
         public string FirstName { get; set; }
         [Column("last_name")]
         public string LastName { get; set; }
