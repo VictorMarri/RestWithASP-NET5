3d2493ab4f81faf4c742b725d202884557bcecd0
VictorMarri <victorpujoni@gmail.com>
2021-02-12 01:09:15 -0300

    Serialização de JSON customizada adicionada.

--- RestWithASPNETFive/RestWithASPNETFive/RestWithASPNETFive/Data/VO/BookVO.cs
+++ RestWithASPNETFive/RestWithASPNETFive/RestWithASPNETFive/Data/VO/BookVO.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace RestWithASPNETFive.Data.VO
 {
     public class BookVO
     {
+        [JsonIgnore]
         public long Id { get; set; }
+        [JsonPropertyName("autor")]
         public string Author { get; set; }
-        
+        [JsonPropertyName("data_lancamento")]
         public DateTime LaunchDate { get; set; }
-        
+        [JsonPropertyName("preco")]
         public float Price { get; set; }
-        
+        [JsonPropertyName("titulo")]
         public string Title { get; set; }
     }
 }
--- RestWithASPNETFive/RestWithASPNETFive/RestWithASPNETFive/Data/VO/PersonVO.cs
+++ RestWithASPNETFive/RestWithASPNETFive/RestWithASPNETFive/Data/VO/PersonVO.cs
@@ -1,18 +1,20 @@
 ﻿using RestWithASPNETFive.Models.Base;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace RestWithASPNETFive.Data.VO
 {
     public class PersonVO
     {
-
+        [JsonIgnore]
         public long Id { get; set; }
+        [JsonPropertyName("primeiro_nome")]
         public string FirstName { get; set; }
-        
+        [JsonPropertyName("ultimo_nome")]
         public string LastName { get; set; }
-        
+        [JsonPropertyName("endereco")]
         public string Address { get; set; }
-       
+        [JsonPropertyName("sexo")]
         public string Gender { get; set; }
     }
 }
