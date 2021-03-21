23d588faadfde152d0779d37cb9e4ad805702ef0
Victor Marri <victor.marri@dtidigital.com.br>
2021-03-21 01:56:35 -0300

    Implementação de autenticação iniciada

--- RestWithASPNETFive/RestWithASPNETFive/RestWithASPNETFive/Repository/UserRepository.cs
+++ RestWithASPNETFive/RestWithASPNETFive/RestWithASPNETFive/Repository/UserRepository.cs
@@ -26,6 +26,38 @@ namespace RestWithASPNETFive.Repository
             return _context.Users.FirstOrDefault(u => (u.UserName.Equals(user.UserName) && (u.Password.Equals(user.Password))));
         }
 
+        public User ResfreshUserInfo(User user)
+        {
+            //Se nao encontrar ninguem no banco com o msm ID passado em parametro, retorna nulo
+            if (!_context.Users.Any(u => u.Id.Equals(user.Id)));
+            {
+                return null;
+            }
+
+            //Se ele encontrar alguem que tenha o mesmo ID, armazena em result
+            var result = _context.Users.SingleOrDefault(p => p.Id.Equals(user.Id));
+
+            //Se result for diferente de nulo, ou seja, existir, a gente atualiza os dados do usuario
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(user);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+                return result;
+            }
+
+            return result;
+
+        }
+
         private string ComputeHash(string input, SHA256CryptoServiceProvider algorithm)
         {
             Byte[] inputBytes = Encoding.UTF8.GetBytes(input);
---
+++ RestWithASPNETFive/RestWithASPNETFive/RestWithASPNETFive/Services/ITokenService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETFive.Services
+{
+    public interface ITokenService
+    {
+        string GenerateAccessToken(IEnumerable<Claim> claims);
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetPrincipalExpiredToken(string token);
+    }
+}
