80c03d2816e190403114eb32a6fe2f47dedc1618
Victor Marri <victor.marri@dtidigital.com.br>
2021-03-29 19:57:14 -0300

    LoginBusinessImplementation feita, VO de Token criado

---
+++ RestWithASPNETFive/RestWithASPNETFive/RestWithASPNETFive/Business/ILoginBusiness.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNETFive.Data.VO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETFive.Business
+{
+    public interface ILoginBusiness
+    {
+        TokenVO ValidateCredentials(UserVO user);
+    }
+}
